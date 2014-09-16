919749302750550287dfa7ca5f541bfdd9a26c91
Nikhil Kaushik <reach_nkaushik@yahoo.com>
2014-09-16 08:29:04 -0500

    Make change to  datbase settings.

--- track_expenses/settings.py
+++ track_expenses/settings.py
@@ -57,12 +57,13 @@ WSGI_APPLICATION = 'track_expenses.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+DATABASES ={}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
