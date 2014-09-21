1e262d35df2d4a6a4d81660aa5e2ea5b15df95d8
sajay <smitha_ajay@yahoo.com>
2014-09-20 21:31:11 -0400

    Renamed model from plural to singular(Expenses to Expense)

--- admintool/models.py
+++ admintool/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Expenses(models.Model):
+class Expense(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     amount_spent = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __unicode__(self):
+        return "Expense " + str(self.id) + " Date : " + str(self.date) + " Amount = " + str(self.amount_spent)
