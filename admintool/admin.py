dc03679e3f66538f240e91839eae2fc338f66b50
Nikhil Kaushik <reach_nkaushik@yahoo.com>
2014-09-21 21:26:35 -0500

    Register all newly added Entities - Expense Category Expense Type and Vendor Type so  that  they  could be rendered through the admin tool

--- admintool/admin.py
+++ admintool/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from admintool.models import ExpenseCategory
+from admintool.models import ExpenseType
+from admintool.models import VendorType
 from admintool.models import Expense
 
+admin.site.register(ExpenseCategory)
+admin.site.register(ExpenseType)
+admin.site.register(VendorType)
 admin.site.register(Expense)
