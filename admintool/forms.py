7e7d82f4e99e097de96307485a05bdc71632ac20
Nikhil Kaushik <reach_nkaushik@yahoo.com>
2014-10-01 15:30:57 -0500

    Add initial value for select box

--- admintool/forms.py
+++ admintool/forms.py
@@ -8,9 +8,9 @@ def clean_amount_spent(self):
     return amount_spent 
 
 class ExpenseForm(forms.Form):
-    expenseCategory = forms.ModelChoiceField(label="Expense Category", queryset=ExpenseCategory.objects.all() , required=False )
-    expenseType =  forms.ModelChoiceField(label="Expense Type", queryset=ExpenseType.objects.all(), required=False ) 
-    vendorType =   forms.ModelChoiceField(label= "Vendor Type" , queryset=VendorType.objects.all(), required=False ) 
+    expenseCategory = forms.ModelChoiceField(label="Expense Category", queryset=ExpenseCategory.objects.all() , required=False, empty_label=None )
+    expenseType =  forms.ModelChoiceField(label="Expense Type", queryset=ExpenseType.objects.all(), required=False, empty_label=None ) 
+    vendorType =   forms.ModelChoiceField(label= "Vendor Type" , queryset=VendorType.objects.all(), required=False,  empty_label=None ) 
     expense_date = forms.DateField(label = "Expense Date",  required=False )
     amount_spent  = forms.DecimalField(label="Amount Spent" , required=False)
     comments =  forms.CharField(label="Comments" , required=False, widget=forms.Textarea)
