abe53d18c8f5bc5e8b8e8f6824c34dad3dc922a5
sajay <smitha_ajay@yahoo.com>
2014-10-27 14:42:30 -0400

    make messages show up in upload target

--- track_expenses/urls.py
+++ track_expenses/urls.py
@@ -8,7 +8,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-#    url(r'^$',include('admintool.urls')),
     url(r'^expenses/', include('admintool.urls')),
 #    url(r'^upload_target/$', 'admintool.views.upload_target'),
     url(r'^login/$', 'django.contrib.auth.views.login'),
