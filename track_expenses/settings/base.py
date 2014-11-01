f6be1bdef5290c0364f7665988d5eb2c9dba2d2c
sajay <smitha_ajay@yahoo.com>
2014-11-01 07:35:21 -0400

    secrets removed

--- track_expenses/settings/base.py
+++ track_expenses/settings/base.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b#wxrs2$cpt85_dme)5*nit(&n0w%rq83k)(deuug&rnt(gi!z'
+SECRET_KEY = os.environ["EXPENSES_SECRET_KEY"]
 
 
 # False by default in base.py
