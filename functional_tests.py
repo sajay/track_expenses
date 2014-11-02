0f8d86cefb5e4b0f8a35168031c788bae3c76426
Smitha <smitha_ajay@yahoo.com>
2014-11-01 20:43:23 -0400

    first functional test with selenium

---
+++ functional_tests.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+import unittest
+
+
+class NewUserTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_sees_home_page(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Welcome to Track Expenses', self.browser.title)
+
+if __name__ == '__main__':
+    unittest.main()
