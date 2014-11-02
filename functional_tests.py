3f20641eb1e535cdc2910c318412ace56f9cff52
Smitha <smitha_ajay@yahoo.com>
2014-11-01 20:50:59 -0400

    first functional test with selenium with alternate way to run tests

--- functional_tests.py
+++ functional_tests.py
@@ -15,5 +15,12 @@ class NewUserTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
         self.assertIn('Welcome to Track Expenses', self.browser.title)
 
-if __name__ == '__main__':
-    unittest.main()
+# This is the simplest way to run this test
+# if __name__ == '__main__':
+#    unittest.main()
+
+# Below is the alternate way to run this test
+# Refer https://docs.python.org/2/library/unittest.html
+
+suite = unittest.TestLoader().loadTestsFromTestCase(NewUserTest)
+unittest.TextTestRunner(verbosity=2).run(suite)
