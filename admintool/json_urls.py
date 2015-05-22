02dd5a0e535096040dca1d46367dd528ad46ee7c
Nikhil Kaushik <reach_nkaushik@yahoo.com>
2015-05-22 12:18:25 -0500

    Added urls to  support Search  functionality

--- admintool/json_urls.py
+++ admintool/json_urls.py
@@ -7,4 +7,7 @@ urlpatterns = patterns('admintool.json_views',
     url(r'^expense_categories/$', json_views.expenseCategory_list, name = 'expenseCategory_list' ),
     url(r'^expense_categories/(?P<pk>[0-9]+)$', json_views.expenseCategory_detail, name ='expenseCategory_detail' ),   
     url(r'^expense/(?P<vendor_name>.+)/$',  json_views.expense_filterByVendor,  name='expense_filterByVendor' ), 
+    url(r'^expenseByDate/([0-9]{4})/([0-9]{2})/$', json_views.expense_filterByDate, name='expense_filterByDate'),
+    url(r'^expenseByDateVendor/([0-9]{4})/([0-9]{2})/([A-Za-z]+)/$', json_views.expense_filterByDateVendor, name='expense_filterByDateVendor'),
+
 )
