b06f91fc38968b018119f66bb4be17e96a5b9f81
Nikhil Kaushik <reach_nkaushik@yahoo.com>
2015-05-22 12:17:48 -0500

    Coded a few Search option

--- admintool/json_views.py
+++ admintool/json_views.py
@@ -5,6 +5,7 @@ from rest_framework import permissions
 from admintool.models import ExpenseCategory, ExpenseType, VendorType, Expense, ExpenseTarget
 from admintool.serializers import ExpenseSerializer,ExpenseCategorySerializer
 from rest_framework import mixins, generics
+from datetime import datetime
 
 @api_view(['GET', 'POST'])
 def expenseCategory_list(request):
@@ -64,6 +65,11 @@ class ExpenseDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin,mixins.De
     def delete(self, request, *args, **kwargs): 
         return self.destroy(request, *args, **kwargs)
 
+
+"""
+ The view below filter expenses based on vendor type
+ex:  http://127.0.0.1:8000/api/v1/expense/Walmart 
+"""
 @api_view(['GET', 'POST', 'DELETE'])
 def expense_filterByVendor( request, vendor_name):
     try:
@@ -76,3 +82,46 @@ def expense_filterByVendor( request, vendor_name):
     if request.method == 'GET':
         serializer = ExpenseSerializer(exp, many=True) 
         return Response(serializer.data)
+
+"""
+  The view below filters expenses based on the month entered
+ex: http://127.0.0.1:8000/api/v1/expense/2014/10   
+"""
+@api_view(['GET', 'POST', 'DELETE'])
+def expense_filterByDate( request, yr, mth ):
+    try:
+        print "Into expense_filterByDate" 
+        print "Year is :" + yr 
+        print "Month is :" + mth 
+#        exp = Expense.objects.filter(created_on__strftime("%Y%m" ) = yr_m ) 
+        exp = Expense.objects.filter(expense_date__year=yr,expense_date__month=mth )
+    except Expense.DoesNotExist:    
+        return Response( status=status.HTTP_404_NOT_FOUND) 
+
+    if request.method == 'GET':
+        serializer = ExpenseSerializer(exp, many=True) 
+        return Response(serializer.data)
+
+"""
+  The view below filters expenses based on year month and Vendor
+ex : http://127.0.0.1:8000/api/v1/expense/2014/10/Walmart
+"""
+@api_view(['GET', 'POST', 'DELETE']) 
+def expense_filterByDateVendor( request, yr, mth, vendor_name ):
+    try:
+        print "Into expense_filterByDateVendor" 
+        print "Year is :" + yr 
+        print "Month is :" + mth 
+        print "Vendor is :" + vendor_name 
+#        exp = Expense.objects.filter(created_on__strftime("%Y%m" ) = yr_m ) 
+        vendor = VendorType.objects.filter(vendor_name=vendor_name)
+        exp = Expense.objects.filter(expense_date__year=yr,expense_date__month=mth,  vendorType_id=vendor[0].id )
+    except Expense.DoesNotExist:    
+        return Response( status=status.HTTP_404_NOT_FOUND) 
+
+    if request.method == 'GET':
+        print "Into  Get" 
+        serializer = ExpenseSerializer(exp, many=True) 
+        return Response(serializer.data)
+
+
