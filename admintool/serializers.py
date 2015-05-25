2ba279e8ea9492beb52db3d9b69c493036e594a1
Nikhil Kaushik <reach_nkaushik@yahoo.com>
2015-05-25 07:09:44 -0500

    added foreign key  relationships

--- admintool/serializers.py
+++ admintool/serializers.py
@@ -1,16 +1,28 @@
 from rest_framework import serializers
-from admintool.models import Expense,ExpenseCategory
+from admintool.models import Expense,ExpenseCategory, ExpenseType, VendorType
 
 class ExpenseCategorySerializer( serializers.ModelSerializer):
     class Meta:
         model=ExpenseCategory
         fields=('id',  'category_name', 'category_desc' )
-   # pk = serializers.IntegerField(read_only=True)
-   # category_name=serializers.CharField( required=True, max_length=25)
-   # category_desc=serializers.CharField( required=False, max_length=50)
+
+class ExpenseTypeSerializer( serializers.ModelSerializer):
+    class Meta:
+        model=ExpenseType
+        fields=('id', 'type_name', 'type_desc' )
+
+class VendorTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=VendorType
+        fields=('id' , 'vendor_name', 'vendor_desc')
 
 class ExpenseSerializer( serializers.ModelSerializer):
+    expenseCategory = serializers.RelatedField(source='ExpenseCategory')
+    expenseType = serializers.RelatedField(source='ExpenseType')
+    vendorType = serializers.RelatedField(source='VendorType')
+    expCategory=serializers.RelatedField(source='expenseCategory.category_name')
+    expType = serializers.RelatedField(source='expenseType.type_name')
+    venType = serializers.RelatedField(source='vendorType.vendor_name')
     class Meta:
         model=Expense
-        fields=('id','expenseCategory','expenseType','amount_spent','expense_date', 'vendorType','created_by')
-
+        fields=('expCategory', 'expType','venType',  'amount_spent', 'expense_date') 
